4c52990d203f8b206dafd0401782f9ba32cf2a04
anuktha11 <145897185+anuktha11@users.noreply.github.com>
2024-08-22 17:35:10 +0530

    Update Header.jsx
    chang

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,11 +7,11 @@ function Header() {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <a className="navbar-brand" href="#home"><b>Bridal Glow</b></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a className="nav-link" href="#home">Home</a>
               </li>
